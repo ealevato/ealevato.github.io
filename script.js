508d2f1358d8de29f439b88c837f87d2ab4ad724
Emanuel <emanuel.alevato@gmail.com>
2024-04-04 12:43:30 -0300

    Test pagina con lista

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-function parseCSV(csvData) {
-  const lines = csvData.split("\n");
-  const data = [];
-
-  for (let line of lines) {
-    const row = line.split(";");
-    data.push(row);
-  }
-  // Pass the data to a variable in your HTML
-  const jsonData = JSON.stringify(data);
-
-  // console.log(jsonData);
-  generateTableRows(jsonData);
-
-  return jsonData;
-}
-
-function fetchAndParseCSV() {
-  fetch("tarifas.csv")
-    .then((response) => response.text())
-    .then((csvData) => {
-      parseCSV(csvData);
-    })
-    .catch((error) => console.error("Error fetching CSV file:", error));
-}
+// Cuadro Tarifario
+let cuadroTarifario = [
+  {
+    categoria: "residencial",
+    consumoDesde: 0,
+    consumoHasta: 50,
+    precioCF: 10,
+    precioCV: 1,
+  },
+  {
+    categoria: "comercial",
+    consumoDesde: 0,
+    consumoHasta: 50,
+    precioCF: 20,
+    precioCV: 2,
+  },
+  {
+    categoria: "tarifa-social",
+    consumoDesde: 0,
+    consumoHasta: 50,
+    precioCF: 5,
+    precioCV: 0.5,
+  },
+];
 
 function calcularConsumo() {
   var categoria = document.getElementById("categoria").value;
@@ -31,46 +30,55 @@ function calcularConsumo() {
 
   if (categoria === "residencial") {
     if (consumo < 50) {
-      resultado = consumo * precioResidencial50;
+      resultado = consumo * cuadroTarifario[0].precioCV;
     } else {
-      resultado = consumo * precioResidencial100;
+      resultado = consumo * cuadroTarifario[0].precioCF;
     }
   } else if (categoria === "comercial") {
     if (consumo < 50) {
-      resultado = consumo * precioComercial50;
+      resultado = consumo * cuadroTarifario[1].precioCV;
     } else {
-      resultado = consumo * precioComercial100;
+      resultado = consumo * cuadroTarifario[1].precioCF;
     }
   } else if (categoria === "tarifa-social") {
     if (consumo < 50) {
-      resultado = consumo * precioTarifaSocial50;
+      resultado = consumo * cuadroTarifario[2].precioCV;
     } else {
-      resultado = consumo * precioTarifaSocial100;
+      resultado = consumo * cuadroTarifario[2].precioCF;
     }
   }
 
   document.getElementById("total").innerHTML = "Total: " + resultado;
 }
 
-// Function to generate table rows
-function generateTableRows(jsonData) {
+function generarTabla() {
   var tableBody = document.getElementById("tarifas-body");
 
   tableBody.innerHTML = "";
 
-  var data = JSON.parse(jsonData);
-
-  for (var i = 1; i < data.length; i++) {
+  for (var i = 0; i < cuadroTarifario.length; i++) {
     var row = document.createElement("tr");
 
-    for (var j = 0; j < data[i].length; j++) {
-      var cell = document.createElement("td");
-      cell.textContent = data[i][j];
-      row.appendChild(cell);
-    }
+    var cellCategoria = document.createElement("td");
+    cellCategoria.textContent = cuadroTarifario[i].categoria;
+    row.appendChild(cellCategoria);
+
+    var cellConsumoDesde = document.createElement("td");
+    cellConsumoDesde.textContent = cuadroTarifario[i].consumoDesde;
+    row.appendChild(cellConsumoDesde);
+
+    var cellConsumoHasta = document.createElement("td");
+    cellConsumoHasta.textContent = cuadroTarifario[i].consumoHasta;
+    row.appendChild(cellConsumoHasta);
+
+    var cellPrecioCF = document.createElement("td");
+    cellPrecioCF.textContent = cuadroTarifario[i].precioCF;
+    row.appendChild(cellPrecioCF);
+
+    var cellPrecioCV = document.createElement("td");
+    cellPrecioCV.textContent = cuadroTarifario[i].precioCV;
+    row.appendChild(cellPrecioCV);
 
     tableBody.appendChild(row);
   }
 }
-
-window.addEventListener("load", fetchAndParseCSV);
---
+++ script2.js
@@ -0,0 +1,76 @@
+// function parseCSV(csvData) {
+//   const lines = csvData.split("\n");
+//   const data = [];
+
+//   for (let line of lines) {
+//     const row = line.split(";");
+//     data.push(row);
+//   }
+//   // Pass the data to a variable in your HTML
+//   const jsonData = JSON.stringify(data);
+
+//   // console.log(jsonData);
+//   generateTableRows(jsonData);
+
+//   return jsonData;
+// }
+
+// function fetchAndParseCSV() {
+//   fetch("tarifas.csv")
+//     .then((response) => response.text())
+//     .then((csvData) => {
+//       parseCSV(csvData);
+//     })
+//     .catch((error) => console.error("Error fetching CSV file:", error));
+// }
+
+// function calcularConsumo() {
+//   var categoria = document.getElementById("categoria").value;
+//   var consumo = document.getElementById("consumo").value;
+//   var resultado;
+
+//   if (categoria === "residencial") {
+//     if (consumo < 50) {
+//       resultado = consumo * precioResidencial50;
+//     } else {
+//       resultado = consumo * precioResidencial100;
+//     }
+//   } else if (categoria === "comercial") {
+//     if (consumo < 50) {
+//       resultado = consumo * precioComercial50;
+//     } else {
+//       resultado = consumo * precioComercial100;
+//     }
+//   } else if (categoria === "tarifa-social") {
+//     if (consumo < 50) {
+//       resultado = consumo * precioTarifaSocial50;
+//     } else {
+//       resultado = consumo * precioTarifaSocial100;
+//     }
+//   }
+
+//   document.getElementById("total").innerHTML = "Total: " + resultado;
+// }
+
+// // Function to generate table rows
+// function generateTableRows(jsonData) {
+//   var tableBody = document.getElementById("tarifas-body");
+
+//   tableBody.innerHTML = "";
+
+//   var data = JSON.parse(jsonData);
+
+//   for (var i = 1; i < data.length; i++) {
+//     var row = document.createElement("tr");
+
+//     for (var j = 0; j < data[i].length; j++) {
+//       var cell = document.createElement("td");
+//       cell.textContent = data[i][j];
+//       row.appendChild(cell);
+//     }
+
+//     tableBody.appendChild(row);
+//   }
+// }
+
+// window.addEventListener("load", fetchAndParseCSV);
